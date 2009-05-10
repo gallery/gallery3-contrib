31d64d66834fd98317826c03e8bc5312bffec003
Bharat Mediratta <bharat@menalto.com>
2009-05-10 21:21:57 +0000

    Make the object take up the whole screen so that it doesn't get cut off

--- modules/developer/views/mptt_tree.html.php
+++ modules/developer/views/mptt_tree.html.php
@@ -1,13 +1,15 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div id="gMPTTTree">
   <h2>
-    <?= t("MPTT Tree Visuallizer") ?>
+    <?= t("MPTT Tree Visualizer") ?>
   </h2>
   <div id="gMPTT">
   <? if (empty($url)): ?>
     <pre><?= $tree ?></pre>
   <? else: ?>
-    <object type="image/svg+xml" data="<?= $url ?>" style="width: 100%" ><pre><?= $tree ?></pre></object>
-  <? endif ?>     
+    <object type="image/svg+xml" data="<?= $url ?>" style="width: 100%; height: 100%;" >
+      <pre><?= $tree ?></pre>
+    </object>
+  <? endif ?>
   </div>
 </div>
