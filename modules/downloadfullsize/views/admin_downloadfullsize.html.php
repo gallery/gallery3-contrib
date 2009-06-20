8cf6d6a4e99f01ae9361151cd055f381b8c18693
rWatcher <rwatcher@australia.edu>
2009-06-20 16:52:47 -0400

    Initial Commit.

---
+++ modules/downloadfullsize/views/admin_downloadfullsize.html.php
@@ -0,0 +1,5 @@
+<?php defined("SYSPATH") or die("No direct script access.") ?>
+<div id="gDownloadFullsizeAdmin">
+  <h2> <?= t("Download Fullsize Links") ?> </h2>
+  <?= $downloadlinks_form ?>
+</div>
---
+++ modules/downloadfullsize/views/downloadfullsize_block.html.php
@@ -0,0 +1,7 @@
+<?php defined("SYSPATH") or die("No direct script access.") ?>
+
+<div class="gDownloadFullsizeBlock">
+<a href="<?= url::site("downloadfullsize/send/$theme->item")
+ ?>" title="<?= t("Download Photo") ?>"
+  class="gButtonLink ui-icon-left ui-state-default ui-corner-all"><?= t("Download Fullsize Image") ?></a>
+</div>
