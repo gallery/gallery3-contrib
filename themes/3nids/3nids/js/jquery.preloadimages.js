b0ad4fa7e765720aac472e57b7f2948c211a03ef
3nids <denis.rouzaud@gmail.com>
2009-09-25 20:18:25 +0200

    Missing js file

---
+++ themes/3nids/3nids/js/jquery.preloadimages.js
@@ -0,0 +1,10 @@
+preload_image_object = '';
+var img_count = 0;
+
+jQuery.preloadImages = function()
+{
+  for(var i = 0; i<arguments.length; i++)
+  {
+    jQuery("<img>").attr("src", arguments[i]);
+  }
+}
