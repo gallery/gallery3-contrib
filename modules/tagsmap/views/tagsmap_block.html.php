2c01cd2fdd275730576beb2c8e20c2dd98996176
rWatcher <rwatcher@australia.edu>
2009-10-15 04:43:19 +0800

    Merged in some code from the 3nids version of TagsMap and the bugfix from glip.

--- modules/tagsmap/views/tagsmap_block.html.php
+++
@@ -1,7 +0,0 @@
-<?php defined("SYSPATH") or die("No direct script access.") ?>
-<ul id="g-tags-map">
-  <li>
-    <a href="<?= url::site("tagsmap/googlemap") ?>">
-    <?= t("Show Map") ?></a>
-  </li>
-</ul>
