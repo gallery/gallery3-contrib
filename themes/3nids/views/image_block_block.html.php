e8302ffcc8cbd10d7ab3f465d36e95a5092ce095
Bharat Mediratta <bharat@menalto.com>
2009-11-27 15:14:21 -0800

    Rename "theme_3nids" to just "3nids" since we no longer have a theme_3nids module.

--- themes/3nids/views/image_block_block.html.php
+++ themes/3nids/views/image_block_block.html.php
@@ -1,3 +1,3 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 
-<?= theme_3nids::fancylink($item, "dynamic", false, false, "h4") ?>
+<?= 3nids::fancylink($item, "dynamic", false, false, "h4") ?>
