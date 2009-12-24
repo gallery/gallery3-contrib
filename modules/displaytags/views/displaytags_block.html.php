9a1fc572117d4f59041b3ded261572d5293cb7a6
Bharat Mediratta <bharat@menalto.com>
2009-12-24 11:04:25 -0800

    Don't treat objects like strings.  Simplify the code.

--- modules/displaytags/views/displaytags_block.html.php
+++ modules/displaytags/views/displaytags_block.html.php
@@ -1,10 +1,8 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div class="g-display-tags-block">
-  <? for ($counter=0; $counter<count($tags); $counter++) { ?>
-    <? if ($counter < count($tags)-1) { ?>
-      <a href="<?= url::site("tags/$tags[$counter]") ?>"><?= html::clean($tags[$counter]->name) ?></a>,
-    <? } else {?>
-      <a href="<?= url::site("tags/$tags[$counter]") ?>"><?= html::clean($tags[$counter]->name) ?></a>
-    <? } ?>
-  <? } ?>
+  <? $not_first = 0; ?>
+  <? foreach ($tags as $tag): ?>
+  <?= ($not_first++) ? "," : "" ?>
+  <a href="<?= url::site("tags/{$tag->name}") ?>"><?= html::clean($tag->name) ?></a>
+  <? endforeach ?>
 </div>
