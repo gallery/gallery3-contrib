35c21708392a92b10bc423d3398b9250ce0912f8
rWatcher <rwatcher@australia.edu>
2010-06-14 18:37:43 -0400

    Added a few '/''s to make XHTML 1.0 Transitional.

--- modules/nobots/views/nobots_block.html.php
+++ modules/nobots/views/nobots_block.html.php
@@ -3,5 +3,5 @@
 <meta name="robots" content="noindex, nofollow, noarchive" /> 
 <meta name="googlebot" content="noindex, nofollow, noarchive, nosnippet, noodp, noimageindex, notranslate" /> 
 <meta name="slurp" content="noindex, nofollow, noarchive, nosnippet, noodp, noydir" /> 
-<meta name="msnbot" content="noindex, nofollow, noarchive, nosnippet, noodp">
-<meta name="teoma" content="noindex, nofollow, noarchive">
+<meta name="msnbot" content="noindex, nofollow, noarchive, nosnippet, noodp" />
+<meta name="teoma" content="noindex, nofollow, noarchive" />
