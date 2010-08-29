8cf229ee72d6129b2746e519ac40a3efc41d4a80
Bharat Mediratta <bharat@menalto.com>
2010-08-28 21:42:35 -0700

    Update greydragon to the latest as of Aug-28

--- themes/greydragon/views/info_block.html.php
+++ themes/greydragon/views/info_block.html.php
@@ -1,9 +1,24 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-
-<span class="g-metadata">
-  <? if ($item->description): ?>
-     <?= bb2html(html::purify($item->description), 1) ?>
-  <? else: ?>
-     &nbsp;
+<ul>
+  <? if ($item->owner): ?>
+  <li>
+    <strong class="caption"><?= t("Owner:") ?></strong>
+    <? if ($item->owner->url): ?>
+    <a href="<?= $item->owner->url ?>"><?= html::clean($item->owner->display_name()) ?></a>
+    <? else: ?>
+    <?= html::clean($item->owner->display_name()) ?>
+    <? endif ?>
+  </li>
   <? endif ?>
-</span>
+  <? if ($item->captured): ?>
+  <li>
+    <strong class="caption"><?= t("Captured:") ?></strong>
+    <?= date("M j, Y H:i:s", $item->captured)?>
+  </li>
+  <? endif ?>
+  <? if ($item->description): ?>
+  <li class="g-description">
+     <?= $theme->bb2html(html::purify($item->description), 1) ?>
+  </li>
+  <? endif ?>
+</ul>
--- themes/greydragon/views/photo.html.php
+++ themes/greydragon/views/photo.html.php
@@ -1,30 +1,79 @@
-<?php defined("SYSPATH") or die("No direct script access.") ?>
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Grey Dragon Theme - a custom theme for Gallery 3
+ * This theme was designed and built by Serguei Dosyukov,
+ * whose blog you will find at http://blog.dragonsoft.us/
+ * Copyright (C) 2009-2010 Serguei Dosyukov
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+?>
+<? if ($theme->desc_allowbbcode): ?>
+  <? $_description = $theme->bb2html($item->description, 1); ?>
+<? else: ?>
+  <? $_description = nl2br(html::purify($item->description)); ?>
+<? endif; ?>
+
+<? if ($theme->is_photometa_visible): ?>
+<?   $_description .= '<ul class="g-metadata">' . $theme->thumb_info($item) . '</ul>'; ?>
+<? endif; ?>
 
 <div id="g-item">
-  <?= $theme->photo_top() ?>
-
+  <? $_title = $theme->bb2html(html::purify($item->title), 1); ?>
   <div id="g-info">
-    <h1><?= html::purify($item->title) ?></h1>
-    <div class="g-hideitem"><?= bb2html(html::purify($item->description), 1) ?></div>
+    <h1><?= $_title ?></h1>
   </div>
-<? if (module::get_var("th_greydragon", "photonav_top")): ?>
-<?= $theme->paginator() ?>
-<? endif ?>
-
+  <?= $theme->add_paginator("top"); ?>
+  <?= $theme->photo_top() ?>
+  <? if (($theme->photo_descmode == "top") and ($_description)): ?>
+    <div id="g-info"><div class="g-description"><?= $_description ?></div></div>
+  <? endif; ?>
   <div id="g-photo">
     <?= $theme->resize_top($item) ?>
     <? if (access::can("view_full", $item)): ?>
-       <a href="<?= $item->file_url() ?>" rel="shadowbox;player=img" title="<?= html::purify($item->title) ?>">
-    <? endif ?>
-    <?= $item->resize_img(array("id" => "g-photo-id-{$item->id}", "class" => "g-resize")) ?>
-    <? if (access::can("view_full", $item)): ?>
+    <? $_url = $item->file_url() . '" class="g-sb-preview" '; ?>
+    <? else: ?>
+    <?  $_url = '#"'; ?>
+    <? endif; ?>
+    <? $_resizewidth = $item->resize_width; ?>
+    <? $siblings = $item->parent()->children(); ?>
+    <? $siblings_count = count($siblings) - 1; ?>
+    <div class="g-resize" style="margin-left: -<?= $_resizewidth / 2; ?>px; ">         
+      <? for ($i = 0; (($i <= $siblings_count) and ($siblings[$i]->rand_key != $item->rand_key)); $i++):
+          ?><a title="<?= $theme->bb2html(html::purify($siblings[$i]->title), 1) ?>" class="g-sb-preview" style="display: none;" href="<?= $siblings[$i]->file_url() ?>">&nbsp;</a><?
+          $siblings_index = $i + 2;
+         endfor; ?>
+      <a href="<?= $_url ?> title="<?= $_title ?>">
+      <?= $item->resize_img(array("id" => "g-photo-id-{$item->id}", "class" => "g-resize", "alt" => $_title)) ?>
       </a>
-    <? endif ?>
+      <? for ($i = $siblings_index; $i <= $siblings_count; $i++):
+          ?><a title="<?= $theme->bb2html(html::purify($siblings[$i]->title), 1) ?>" class="g-sb-preview" style="display: none;" href="<?= $siblings[$i]->file_url() ?>">&nbsp;</a><?
+         endfor; ?>
+      <? if (($theme->photo_descmode == "overlay") and ($_description)): ?>
+        <span class="g-more">More</span>
+        <span class="g-description" style="width: <?= $_resizewidth - 20; ?>px;" >
+          <strong><?= $_title ?></strong>
+          <?= $_description ?>
+        </span>
+      <? endif ?>
+    </div>
     <?= $theme->resize_bottom($item) ?>
   </div>
-  
-<? if (module::get_var("th_greydragon", "photonav_bottom")): ?>
-<?= $theme->paginator() ?>
-<? endif ?>
+  <? if (($theme->photo_descmode == "bottom") and ($_description)): ?>
+    <div id="g-info"><div class="g-description"><?= $_description ?></div></div>
+  <? endif; ?>
+  <?= $theme->add_paginator("bottom"); ?>
   <?= $theme->photo_bottom() ?>
 </div>
