3640bd60bb2f177999c068906ee659160ee2fc68
Bharat Mediratta <bharat@menalto.com>
2010-10-08 12:52:55 -0700

    Get rid of the 3.1/obsolete dir as well as the obsolete rescue module

--- 3.1/obsolete/web_client/system/config/html_purifier.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * If using HTMLPurifier then this option allows setting an 
- * alternative path for the cache data. Note that that path 
- * must be an absolute location.
- */
-$config['cache'] = '/tmp/htmlpurifier';
