e06b145fe5d8a8725ae4ef24c28661bb374e4d4b
Chad Kieffer <ckieffer@gmail.com>
2010-12-23 06:10:27 +0800

    Initial commit of bitly module. Installer and admin settings implemented. URL shortening method works but is not yet implemented for items.

---
+++ 3.1/modules/bitly/helpers/bitly_event.php
@@ -0,0 +1,50 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2010 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+class bitly_event_Core {
+  static function admin_menu($menu, $theme) {
+    $menu->get("settings_menu")
+      ->append(Menu::factory("link")
+        ->id("bitly_menu")
+        ->label(t("Bit.ly"))
+        ->url(url::site("admin/bitly")));
+  }
+
+  static function site_menu($menu, $theme) {
+    $item = $theme->item();
+    if ($item && $item->type == "photo") {
+      $menu->get("options_menu")
+        ->append(Menu::factory("link")
+                    ->id("bitly")
+                    ->label(t("Shorten Link with bit.ly"))
+                    ->url(url::site("bitly/shorten_link/$item->id?csrf=$theme->csrf"))
+                    ->css_id("g-bitly-link")
+                    ->css_class("g-print-bitly-link ui-icon-print"));
+    }
+  }
+
+  static function context_menu($menu, $theme, $item) {
+    $menu->get("options_menu")
+      ->append(Menu::factory("link")
+               ->id("bitly")
+               ->label(t("Shorten Link with bit.ly"))
+               ->url(url::site("bitly/shorten_link/$item->id?csrf=$theme->csrf"))
+               ->css_class("g-bitly-link ui-icon-link"));
+  }
+}
