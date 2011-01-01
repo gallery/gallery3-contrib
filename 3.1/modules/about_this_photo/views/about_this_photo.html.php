7f2ee413f13d020c7d5bcd6b4097ef9183da4107
undagiga <git@cyberhalides.com>
2011-01-01 19:19:23 +1100

    Changes by Undagiga to add selected IPTC fields

--- 3.1/modules/about_this_photo/views/about_this_photo.html.php
+++ 3.1/modules/about_this_photo/views/about_this_photo.html.php
@@ -1,17 +1,34 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<ul class="g-metadata">
-  <li>
-    <strong class="caption"><?= t("Date:") ?></strong>
-    <?= $date ?>
-  </li>
-  <li>
-    <strong class="caption"><?= t("Time:") ?></strong>
-    <?= $time ?>
-  </li>
-  <li>
-    <strong class=="caption"><?= t("Tags:") ?></strong>
+<? date_default_timezone_set('Australia/ACT'); ?> 
+<div class="g-metadata">
+<span class="g-about-this">
+<table cellspacing="0" cellpadding="0" border="0">
+<tr>
+    <td><strong class="caption"><?= t("Date:&nbsp;") ?></strong></td>
+    <td><?= $date ?></td>
+</tr>
+<tr>
+    <td><strong class="caption"><?= t("Time:&nbsp;") ?></strong></td>
+    <td><?= $time ?></td>
+</tr>
+<tr>
+    <td><strong class="caption"><?= t("Views:&nbsp;") ?></strong></td>
+    <td><?= $vcount ?></td>
+</tr>
+<tr>
+    <td><strong class="caption"><?= t("Image ID:&nbsp;") ?></strong></td>
+    <td><?= $source ?></td>
+</tr>
+</table>
+  <span >
+    <strong class="caption"><?= t("Caption:&nbsp;") ?></strong>
+    <?= $caption ?>
+  </span ><br>
+  <span >
+    <strong class=="caption"><?= t("Tags: &nbsp;&nbsp;") ?></strong>
     <? foreach ($tags as $tag): ?>
-    <a href="<?= $tag->url() ?>"><?= html::clean($tag->name) ?></a>
+    <a href="<?= $tag->url() ?>"><?= html::clean($tag->name) ?></a>,
     <? endforeach?>
-  </li>
-</ul>
+  </span ><br>
+</span>
+</div>
