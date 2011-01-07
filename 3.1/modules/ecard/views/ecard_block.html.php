ee0602c9d935696c767bd22f535c422497bc7076
dmolavi <dmolavi@gmail.com>
2011-01-07 15:36:16 -0500

    Fix for CSS display issues. Closes 1583

--- 3.1/modules/ecard/views/ecard_block.html.php
+++ 3.1/modules/ecard/views/ecard_block.html.php
@@ -1,6 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<a href="<?= url::site("ecard/form_send/{$item->id}") ?>" id="g-send-ecard"
+<a href="<?= url::site("ecard/form_send/{$item->id}") ?>" 
    class="g-dialog-link g-button ui-state-default ui-corner-all">
-  <span class="ui-icon-ecard"></span>
+  <span class="ui-icon-ecard" id="g-send-ecard"></span>
   <?= t("Send as eCard") ?>
 </a>
