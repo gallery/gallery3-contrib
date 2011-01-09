b1afdedaf578894bad70ea5e1fac344495336f52
colings <colings@example.com>
2011-01-09 16:41:25 -0600

    Removed all non-picasa_faces stuff from the picasa_faces branch

--- 3.0/modules/custom_albums/models/custom_album.php
+++
@@ -1,5 +0,0 @@
-<?php defined("SYSPATH") or die("No direct script access.");
-class Custom_Album_Model extends ORM
-{
-}
-?>
--- 3.0/themes/clean_canvas_wide/views/no_sidebar.html.php
+++
@@ -1,6 +0,0 @@
-<?php defined("SYSPATH") or die("No direct script access.") ?>
-<ul class="g-message-block">
-  <li class="g-warning"><?= t("No active sidebar blocks.") ?>
-    <br/><a href="<?= url::site("admin/sidebar") ?>"><?= t("Add blocks") ?></a>
-  </li>
-</ul>
