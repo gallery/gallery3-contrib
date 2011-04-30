4175d8d5845e798195d572e48d3662c85c23349f
Chad Parry <github@chad.parry.org>
2011-04-30 16:09:29 -0600

    Change the name of the extensions helper to legal_file.

--- 3.0/modules/rawphoto/helpers/rawphoto_event.php
+++ 3.0/modules/rawphoto/helpers/rawphoto_event.php
@@ -39,7 +39,7 @@ class rawphoto_event_Core {
          ->url(url::site("admin/rawphoto")));
   }
 
-  static function upload_extensions($extensions_wrapper) {
+  static function legal_file_extensions($extensions_wrapper) {
     array_push($extensions_wrapper->extensions,
                "bay", "bmq", "cr2", "crw", "cs1", "dc2", "dcr", "dng", "fff", "k25", "kdc",
                "mos", "mrw", "nef", "orf", "pef", "raf", "raw", "rdc", "srf", "x3f");
