49c7e6f1c69edf36cccdbcddf16c6f97bffe2ef3
rWatcher <rwatcher@watcherdatabase.tk>
2011-06-12 02:17:46 -0400

    Initial commit of the Pages module.

---
+++ 3.1/modules/pages/views/pages_display.html.php
@@ -0,0 +1,7 @@
+<?php defined("SYSPATH") or die("No direct script access.") ?>
+<div class="g-page-block">
+  <h1> <?= $title ?> </h1>
+  <div class="g-page-block-content">
+    <?=$body ?>
+  </div>
+</div>
