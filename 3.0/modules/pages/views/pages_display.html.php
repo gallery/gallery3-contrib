412bc0d7ddfce7915c3bf287e81aca9e2dee79f6
rWatcher <rwatcher@watcherdatabase.tk>
2011-06-22 01:26:49 -0400

    Added an option for hiding the sidebar on Pages, as per forum request.

--- 3.0/modules/pages/views/pages_display.html.php
+++ 3.0/modules/pages/views/pages_display.html.php
@@ -1,4 +1,14 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
+<? if (module::get_var("pages", "show_sidebar")) : ?>
+  <style type="text/css">
+  #g-sidebar {
+    display: none;
+  }
+  #g-content {
+    width: 950px;
+  }
+  </style>
+<? endif ?>
 <div class="g-page-block">
   <h1> <?= $title ?> </h1>
   <div class="g-page-block-content">
