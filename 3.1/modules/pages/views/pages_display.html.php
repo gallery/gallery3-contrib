2eb24a21b54ce7a024b0320ca660db0b7582ddb0
rWatcher <rwatcher@watcherdatabase.tk>
2011-06-22 10:54:58 -0400

    grey_dragon support.

--- 3.1/modules/pages/views/pages_display.html.php
+++ 3.1/modules/pages/views/pages_display.html.php
@@ -1,13 +1,23 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <? if (module::get_var("pages", "show_sidebar")) : ?>
   <style type="text/css">
-  #g-sidebar {
-    display: none;
-  }
-  #g-content {
-    width: 950px;
-  }
+    <? if (module::get_var("gallery", "active_site_theme") == "greydragon") : ?>
+    #g-column-right {
+      display: none;
+    }
+    .g-page-block-content {
+      width: 99%;
+    }
+    <? else: ?>
+    #g-sidebar {
+      display: none;
+    }
+    #g-content {
+      width: 950px;
+    }
+    <? endif ?>
   </style>
+  <? print module::get_var("gallery", "active_site_theme"); ?>
 <? endif ?>
 <div class="g-page-block">
   <h1> <?= $title ?> </h1>
