daac27904d57d4c172364027ac8cd90f5e65de46
brentil <forums@inner-ninja.com>
2011-06-24 18:00:58 -0400

    v8 - 2011.06.23 Fixed: Bugs relating to the new Community Contributed Modules section & GCC stored modules Added: Modules information for the new Gallery 3.0.2 Modules tab Added: SPEED! The module checking process is now 60% faster than before thanks to logic updates in the data gathering process. The decrease in update checking time will be more noticeable as more modules are added.

--- modules/moduleupdates/helpers/moduleupdates_installer.php
+++ modules/moduleupdates/helpers/moduleupdates_installer.php
@@ -1,6 +1,6 @@
 <?php defined("SYSPATH") or die("No direct script access.");/**
  * Gallery - a web based photo album viewer and editor
- * Copyright (C) 2000-2010 Bharat Mediratta
+ * Copyright (C) 2000-2011 Bharat Mediratta
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,21 +17,35 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
  
+
 class moduleupdates_installer {
 
   static function install() {
   
     $version = module::get_version("moduleupdates");
-    if ($version == 0) {
-      module::set_version("moduleupdates", 1);
+    if ($version < 1) {
+      module::set_version("moduleupdates", 8);
+      //Remove the ModuleUpdates cache entry 'JIC'
+      Cache::instance()->delete("ModuleUpdates");
+      //create the blank ModuleUpdates cache entry with an expiration of 0 days
+      Cache::instance()->set("moduleupdates_cache", "", array("ModuleUpdates"), null);
+      Cache::instance()->set("moduleupdates_cache_updates", "", array("ModuleUpdates"), null);
     }
   }
 
   static function upgrade($version) {
+    module::set_version("moduleupdates", 8);
+    //Remove the ModuleUpdates cache entry 'JIC'
+    Cache::instance()->delete("ModuleUpdates");
+    //Empty the ModuleUpdates cache entry so our new version starts from scratch
+    Cache::instance()->set("moduleupdates_cache", "", array("ModuleUpdates"), null);
+    Cache::instance()->set("moduleupdates_cache_updates", "", array("ModuleUpdates"), null);
   }
 
   static function uninstall() {
-  
+	
+    //Remove the ModuleUpdates cache entry as we remove the module
+    Cache::instance()->delete("ModuleUpdates");
     module::delete("moduleupdates");
   }
 }
