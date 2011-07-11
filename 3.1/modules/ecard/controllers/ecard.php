dbac57e6fb7ee0c9c6a1eeac545cb42b7e429ceb
Bharat Mediratta <bharat@menalto.com>
2011-07-11 12:01:17 -0700

    Fix indentation

--- 3.1/modules/ecard/controllers/ecard.php
+++ 3.1/modules/ecard/controllers/ecard.php
@@ -1,27 +1,27 @@
 <?php defined("SYSPATH") or die("No direct script access.");
 /**
-* Gallery - a web based photo album viewer and editor
-* Copyright (C) 2000-2011 Bharat Mediratta
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or (at
-* your option) any later version.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2011 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 class Ecard_Controller extends Controller {
 	/**
-* Send the ecard.
-*/
+         * Send the ecard.
+         */
 	public function send($id) {
 		$item = ORM::factory("item", $id);
 		access::required("view", $item);
@@ -38,7 +38,7 @@ class Ecard_Controller extends Controller {
 			}
 			$valid = false;
 		}
-		
+
 		if ($valid) {
 			$v = new View("ecard_email.html");
 			$v->item = $item;
@@ -47,7 +47,7 @@ class Ecard_Controller extends Controller {
 			$bcc = module::get_var("ecard", "bcc");
 			if($form->send_ecard->send_to_self->checked == true) {
 				$cc = $form->send_ecard->inputs["from_email"]->value;
-			}	  
+			}
 			$v->message = t(module::get_var("ecard", "message"), array("fromname" => $from_name));
 			$v->custom_message = $form->send_ecard->text->value;
 			$v->image = $item->name;
@@ -67,7 +67,7 @@ class Ecard_Controller extends Controller {
 					gallery_graphics::resize($item->file_path(), $tmpfile, $options, $item);
 					$mime->addHTMLImage($tmpfile,$item->mime_type,$item->name);
 				}
-			} else {				
+			} else {
 				if($form->send_ecard->send_thumbnail->checked == true) {
 					$mime->addHTMLImage($item->thumb_path(),$item->mime_type,$item->name);
 				} else {
@@ -89,8 +89,8 @@ class Ecard_Controller extends Controller {
 		}
 	}
 	/**
-* Present a form for sending a new ecard.
-*/
+         * Present a form for sending a new ecard.
+         */
 	public function form_send($item_id) {
 		$item = ORM::factory("item", $item_id);
 		access::required("view", $item);
@@ -100,7 +100,7 @@ class Ecard_Controller extends Controller {
 		$v_form = new View("ecard_form.html");
 		$v_form->item_id = $item_id;
 		print $v_form->render();
-	}  
+	}
 	private static function _notify($to, $from, $subject, $item, $text, $headers, $bcc, $cc) {
 		$sendmail = Sendmail::factory();
 		$sendmail
@@ -112,7 +112,7 @@ class Ecard_Controller extends Controller {
 		}
 		if(isset($cc)) {
 			$sendmail->header("cc",$cc);
-		}	  
+		}
 		foreach($headers as $key => $value) {
 			$sendmail->header($key,$value);
 		}
@@ -120,5 +120,5 @@ class Ecard_Controller extends Controller {
 		->message($text)
 		->send();
 		return;
-	}  
+	}
 }
