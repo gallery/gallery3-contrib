063ad6fb7fa0dd0550544d0afc64fb3a0af4afb8
Fredrik Erlandsson <fredrik.e@gmail.com>
2012-01-10 14:12:15 +0100

    move of context menu to top of thumb

--- js/ui.init.js
+++ js/ui.init.js
@@ -41,6 +41,7 @@ $(document).ready(function() {
       var iconClass = $(this).parent().attr("class").match(/ui-icon-.[^\s]+/).toString();
       $(this).addClass(iconClass);
     });
+    $("ul.g-context-menu > li a span").addClass("ui-icon-carat-1-s");
   }
 
   // Remove titles for menu options since we're displaying that text anyway
