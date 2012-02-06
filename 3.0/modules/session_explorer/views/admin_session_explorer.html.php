be6cb880c1bd15c2260e4560fe92f4ae60987179
Bharat Mediratta <bharat@menalto.com>
2012-02-05 16:58:29 -0800

    Improve the session decoding, limit it to a sample size, improve the output.

--- 3.0/modules/session_explorer/views/admin_session_explorer.html.php
+++ 3.0/modules/session_explorer/views/admin_session_explorer.html.php
@@ -1,6 +1,9 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
 <div id="g-admin-session-explorer" class="g-block">
   <h1> <?= t("Top user agents and ips") ?> </h1>
+  <p>
+    <?= t("Numbers are based on a sample size of %sample_size sessions", array("sample_size" => $sample_size)) ?>
+  </p>
 
   <div class="g-block-content">
     <h2> <?= t("User agents") ?> </h2>
@@ -11,8 +14,8 @@
         <th> <?= t("User agent") ?> </th>
       </tr>
 
-      <? foreach ($uas as $ua => $count): ?>
       <? $rank = 0; ?>
+      <? foreach ($uas as $ua => $count): ?>
       <tr class="<?= text::alternate("g-odd", "g-even") ?>">
         <td> <?= ++$rank ?> </td>
         <td> <?= $count ?> </td>
@@ -31,8 +34,8 @@
         <th> <?= t("Internet address") ?> </th>
       </tr>
 
-      <? foreach ($ips as $ip => $count): ?>
       <? $rank = 0; ?>
+      <? foreach ($ips as $ip => $count): ?>
       <tr class="<?= text::alternate("g-odd", "g-even") ?>">
         <td> <?= ++$rank ?> </td>
         <td> <?= $count ?> </td>
