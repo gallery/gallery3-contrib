55e762f102324d980ffa4331e2201c95e5384e6a
Bharat Mediratta <bharat@menalto.com>
2012-02-27 09:50:35 -0800

    Remove 3.1 dir - we're nowhere close to 3.1 and this is just causing confusion.

--- 3.1/modules/basket/views/paypal_redirect.html.php
+++
@@ -1,4 +0,0 @@
-<?= $form?>
-<h1>Processing</h1><h3>If you are not automatically redirected to
- paypal within 5 seconds <a href='javascript:s_f();'>Click Here</a>.</h3>
-
--- 3.1/modules/gwtorganize/war/g3viewer/deferredjs/0D97DF37194D1924CC80394AAA96B9A3/1.cache.js
+++
@@ -1,9 +0,0 @@
-function ZQ(){}
-function WQ(){}
-function dR(){}
-function VQ(){}
-function bR(){return EN}
-function YQ(){return DN}
-function _Q(){_Q=k9;$Q=new WQ}
-function cR(){$Q=(_Q(),new VQ);zx((wx(),vx),1);!!$stats&&$stats(dy(wob,xob,null,null));$Q.Xb();!!$stats&&$stats(dy(wob,yob,null,null))}
-var Bob='AsyncLoader1',Aob='AsyncLoader1__Super',wob='runCallbacks1';_=WQ.prototype=new Gf;_.gC=YQ;_.Xb=ZQ;_.tI=0;_=VQ.prototype=new WQ;_.gC=bR;_.Xb=dR;_.tI=0;var $Q;var DN=R1(zob,Aob),EN=R1(zob,Bob);cR();
