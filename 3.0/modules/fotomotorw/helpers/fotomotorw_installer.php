80194c67529d629fbb993e2cea2ce20e1092d523
rWatcher <rwatcher@watcherdatabase.tk>
2012-05-31 00:05:33 -0400

    Initial commit of Fotomoto rW module.

---
+++ 3.0/modules/fotomotorw/helpers/fotomotorw_installer.php
@@ -0,0 +1,35 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2012 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+class fotomotorw_installer {
+  static function install() {
+    // Set up some default values.
+    module::set_var("fotomotorw", "fotomoto_site_key", '');
+    module::set_var("fotomotorw", "fotomoto_private_key", md5(random::hash() . access::private_key()));
+    module::set_var("fotomotorw", "fotomoto_buy_prints", 1);
+    module::set_var("fotomotorw", "fotomoto_buy_cards", 1);
+    module::set_var("fotomotorw", "fotomoto_buy_download", 1);
+    module::set_var("fotomotorw", "fotomoto_share_ecard", 1);
+    module::set_var("fotomotorw", "fotomoto_share_facebook", 1);
+    module::set_var("fotomotorw", "fotomoto_share_twitter", 1);
+    module::set_var("fotomotorw", "fotomoto_share_digg", 1);
+    module::set_version("fotomotorw", 1);
+  }
+}
---
+++ 3.0/modules/fotomotorw/helpers/fotomotorw_theme.php
@@ -0,0 +1,71 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2012 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+ 
+class fotomotorw_theme_Core {  
+  static function head($theme) {
+    // Load fotomoto's js file on photo pages.
+    if($theme->page_subtype == "photo") {
+      return html::script('http://widget.fotomoto.com/stores/script/' . module::get_var("fotomotorw", "fotomoto_site_key") . '.js?api=true');
+    }
+  }
+
+  static function resize_bottom($theme) {
+    // Generate an array of links to display below photos.
+    $block = new Block;
+    $block->css_id = "g-fotomoto";
+    $block->anchor = "fotomoto";
+
+    $link_array = array();
+    $counter = 0;
+    if (module::get_var("fotomotorw", "fotomoto_buy_prints")) {
+      $link_array[$counter] = array("100", "Buy Prints");
+      $counter++;
+    }
+    if (module::get_var("fotomotorw", "fotomoto_buy_cards")) {
+      $link_array[$counter] = array("300", "Buy Cards");
+      $counter++;
+    }
+    if (module::get_var("fotomotorw", "fotomoto_buy_download")) {
+      $link_array[$counter] = array("400", "Download");
+      $counter++;
+    }
+    if (module::get_var("fotomotorw", "fotomoto_share_ecard")) {
+      $link_array[$counter] = array("200", "Send eCard");
+      $counter++;
+    }
+    if (module::get_var("fotomotorw", "fotomoto_share_facebook")) {
+      $link_array[$counter] = array("201", "Share on Facebook");
+      $counter++;
+    }
+    if (module::get_var("fotomotorw", "fotomoto_share_twitter")) {
+      $link_array[$counter] = array("202", "Share on Twitter");
+      $counter++;
+    }
+    if (module::get_var("fotomotorw", "fotomoto_share_digg")) {
+      $link_array[$counter] = array("203", "Share on Digg");
+      $counter++;
+    }
+
+    $view = new View("fotomotorw_block.html");
+    $view->details = $link_array;
+    $block->content = $view;
+    return $block;
+  }
+}
