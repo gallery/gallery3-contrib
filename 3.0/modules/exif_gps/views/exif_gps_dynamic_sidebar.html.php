fd35b351f198c8c87aacdf7c5c57b84c560b1e10
rWatcher <rwatcher@watcherdatabase.tk>
2012-06-18 20:00:07 -0400

    Exif_GPS Version 3.

--- 3.0/modules/exif_gps/views/exif_gps_dynamic_sidebar.html.php
+++ 3.0/modules/exif_gps/views/exif_gps_dynamic_sidebar.html.php
@@ -1,22 +1,103 @@
 <?php defined("SYSPATH") or die("No direct script access.") ?>
-<script type="text/javascript" src="http://www.google.com/jsapi?key=<?= module::get_var("exif_gps", "googlemap_api_key"); ?>"></script>
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
 <script type="text/javascript">
-  google.load("maps", "3",{"other_params":"sensor=false"});
+<?
+  if (module::get_var("exif_gps", "googlemap_api_key", "") != "") {
+    print "google.load(\"maps\", \"3\",  {other_params:\"key=" . module::get_var("exif_gps", "googlemap_api_key") . "&sensor=false\"});";
+  } else {
+    print "google.load(\"maps\", \"3\",  {other_params:\"sensor=false\"});";
+  }
+?>
+
+  var google_zoom_hack = false;
+  var map = null;
+<?
+  if (isset($album_id)) {
+    print "  var album_id = " . $album_id . ";\n";
+  } else {
+    print "  var album_id = 0;\n";
+  }
+?>
+
+  $.ajaxSetup({
+    error: function(xhr, status, error) {
+      var status_text = document.getElementById('exif-gps-status');
+      status_text.innerHTML = "<font size=\"4\" color=\"white\"><strong>An AJAX error occured: " + status + "<br />\nError: " + error + "</strong></font>";
+    }
+  });
+
   function initialize() {
-    var latlng = new google.maps.LatLng(<?=$latitude; ?>,<?=$longitude; ?>);
+    var myLatlng = new google.maps.LatLng(<?=$latitude; ?>,<?=$longitude; ?>);
     var myOptions = {
-      zoom: <?= module::get_var("exif_gps", "sidebar_zoom"); ?>,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.<?=$sidebar_map_type ?>
-    };
-    var map = new google.maps.Map(document.getElementById("sidebar_map_canvas"), myOptions);
-    var marker = new google.maps.Marker({
-      position: latlng, 
-      map: map
+      zoom: 1,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.<?=$sidebar_map_type; ?>
+    }
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+    var glatlngbounds = new google.maps.LatLngBounds( ); // This is so we can auto center the map.
+
+    var map_markers = [];
+    var current_latlng = null;
+    if (album_id > 0) {
+      jQuery.get("<?=url::abs_site("exif_gps/xml/album/{$album_id}"); ?>", {}, function(data) {
+        jQuery(data).find("marker").each(function() {
+          var xmlmarker = jQuery(this);
+          var latlng = new google.maps.LatLng(parseFloat(xmlmarker.attr("lat")),
+                                      parseFloat(xmlmarker.attr("lng")));
+
+          if (!latlng.equals(current_latlng)) {
+            current_latlng = latlng;
+            glatlngbounds.extend(latlng);
+            var marker = new google.maps.Marker({position: latlng});
+            map_markers.push(marker);
+          }
+        });
+
+        var mcOptions = { gridSize: <?= module::get_var("exif_gps", "markercluster_gridsize"); ?>, maxZoom: <?= module::get_var("exif_gps", "markercluster_maxzoom"); ?>};
+        var markerCluster = new MarkerClusterer(map, map_markers, mcOptions);
+        google_zoom_hack = true;
+        map.fitBounds(glatlngbounds);
+        document.getElementById('over_map').style.display = 'none';
+      });
+    } else {
+      var latlng = new google.maps.LatLng(<?=$latitude; ?>,<?=$longitude; ?>);
+      var marker = new google.maps.Marker({
+        position: latlng, 
+        map: map
+      });
+      map.setZoom(<?= module::get_var("exif_gps", "sidebar_zoom"); ?>);
+      document.getElementById('over_map').style.display = 'none';
+    }
+
+    <? if (($max_autozoom = module::get_var("exif_gps", "googlemap_max_autozoom")) != "") : ?>
+    // If there is a maximum auto-zoom value, then set up an event to check the zoom
+    // level the first time it is changed, and adjust it if necessary.
+    // (if we call map.getZoom right after .fitBounds, getZoom will return the initial 
+    // zoom level, not the auto zoom level, this way we get the auto zoomed value).
+    google.maps.event.addListener(map, 'zoom_changed', function() {
+      if (google_zoom_hack) {
+        if (map.getZoom() > <?= $max_autozoom ?>) map.setZoom(<?= $max_autozoom ?>);
+        google_zoom_hack = false;
+      }
     });
+    <? endif ?>
   }
 
   google.setOnLoadCallback(initialize);
-
 </script>
-<div id="sidebar_map_canvas" style="width:205px; height:214px"><img src="http://maps.google.com/maps/api/staticmap?center=<?=$latitude; ?>,<?=$longitude; ?>&zoom=<?= module::get_var("exif_gps", "sidebar_zoom"); ?>&size=205x214&maptype=<?=$sidebar_map_type ?>&markers=color:red|color:red|<?=$latitude; ?>,<?=$longitude; ?>&sensor=false"></div>
+
+<style>
+   #wrapper { position: relative; }
+   #over_map { position: absolute; top: 0px; left: 0px; z-index: 99; }
+</style>
+
+<div id="wrapper">
+  <div id="map_canvas" style="width:205px; height:214px"></div>
+  <div id="over_map" style="width:205px; height:214px">
+    <p id="exif-gps-status" style="text-align: center; display: table-cell; vertical-align: middle; width:205px; height:214px">
+      <font size="4" color="white"><strong><?= t("Loading..."); ?></strong></font><br /><br />
+      <img src="<?= url::file("modules/exif_gps/images/exif_gps-loading-map-large.gif"); ?>" style="vertical-align: middle;"></img>
+    </p>
+  </div>
+</div>
