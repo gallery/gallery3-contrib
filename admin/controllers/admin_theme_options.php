25ae7f02cd893127901bd26fc1297d109b781746
Fredrik Erlandsson <fredrik.e@gmail.com>
2012-08-16 21:51:01 +0200

    Squashed '3.0/themes/pear4gallery3/' changes from f9f9805..f6e42a9
    f6e42a9 Support for custom size of photo resizes. Version up to 3.5.2
    639cc53 fixes #27, support for image count re-enabled
    fe33691 Merge pull request #32 from oneoftwo/patch-3
    9790a15 Update views/thumbs.html.php
    77408dc Merge pull request #30 from oneoftwo/patch-1
    62f7886 Update views/thumbs.html.php
    25145c0 b3.4 + threaded thumbLoad
    32ddbcc Merge branch 'b3.4'
    5db2a89 new attempt for custom logo
    1b18764 full url to custom logo stored in var
    cd067e4 stupid pear logo..
    d64bdbf Attempt to fix custom logo issues, version up
    9e8ea7f minor fixes for the FB-comment module support
    9af348f version 3.4.3.1
    0e6db7c fix for problematic Tag-Cloud module
    a7b0a70 Fixes issues with facebook plugins when the user is commenting on a photo and FB links to the album image version 3.4.3
    900b94c minor errors
    7954cf4 version up
    d307b65 attempt to fix #17, image prefetch
    015ceca various fixed thanks to e-nando/chris@2243c91c400ff2c18ff40e15b2b355f5429264c5
    4971e20 closes #21, no longer have a discontinuous slideshowImages array
    ac86dec fixes for facebook-comments, now we are using the full url
    efb4fde closes #20, support for the search module in a good way
    7a12558 minor fixes to eleminate some debug errors
    
    git-subtree-dir: 3.0/themes/pear4gallery3
    git-subtree-split: f6e42a99776267d39c074f8c6b4c3ed606e280f7

--- admin/controllers/admin_theme_options.php
+++ admin/controllers/admin_theme_options.php
@@ -84,7 +84,7 @@ class Admin_Theme_Options_Controller extends Admin_Controller {
       ->value(module::get_var("gallery", "appletouchicon_url"));
     $group->input("logo_path")
       ->label(t("URL (relative path) to custom logo"))
-      ->value(module::get_var("gallery", "logo_path"));
+      ->value(module::get_var("th_pear4gallery3", "logo_path"));
     $group->input("slideshow_time")
       ->label(t("Slideshow timeout (in ms)"))
       ->value(module::get_var("th_pear4gallery3", "slideshow_time", "5000"));
@@ -140,6 +140,13 @@ class Admin_Theme_Options_Controller extends Admin_Controller {
           "scale" => t("Shrink and grow an element"),
           "none" => t("Disable effects (faster switching)")))
       ->selected(module::get_var("th_pear4gallery3", "mosaic_effect", "blind"));
+
+    /* Advanced Options - Photo settings ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+    $group = $form->group("edit_theme_adv_photo")->label(t("Advanced Options - Photo settings"));
+    $group->input("resize_size")
+      ->label(t("Photo resize size (800 is default)"))
+      ->value(module::get_var("gallery", "resize_size", "800"));
 /*
     $group->dropdown("photo_descmode")
       ->label(t("Description Display Mode"))
@@ -258,8 +265,10 @@ class Admin_Theme_Options_Controller extends Admin_Controller {
         message::success(t("Theme details are reset"));
       else:
         // * General Settings ****************************************************
+        $old_resize_size  = module::get_var("gallery", "resize_size", 800);
+        module::set_var("gallery", "resize_size", filter_var($form->edit_theme_adv_photo->resize_size->value, FILTER_VALIDATE_INT, array('options' => array('default' => 800, 'min_range' => 640))));
 
-        $resize_size  = 800;
+        $resize_size  = module::get_var("gallery", "resize_size", 800);
 
         $build_resize = $form->maintenance->build_resize->value;
         $build_thumbs = $form->maintenance->build_thumbs->value;
@@ -271,7 +280,7 @@ class Admin_Theme_Options_Controller extends Admin_Controller {
         endif;
         $purge_cache  = $form->maintenance->purge_cache->value;
 
-        if ($build_resize):
+        if ($build_resize || $old_resize_size != $resize_size):
           graphics::remove_rule("gallery", "resize", "gallery_graphics::resize");
           graphics::add_rule("gallery", "resize", "gallery_graphics::resize",
             array("width" => $resize_size, "height" => $resize_size, "master" => Image::AUTO), 100);
@@ -297,7 +306,7 @@ class Admin_Theme_Options_Controller extends Admin_Controller {
         module::set_var("gallery", "page_size", 50);
         module::set_var("gallery", "favicon_url", $form->edit_theme->favicon->value);
         module::set_var("gallery", "appletouchicon_url", $form->edit_theme->appletouchicon->value);
-        module::set_var("gallery", "logo_path", $form->edit_theme->logo_path->value);
+        $this->save_item_state("logo_path", $form->edit_theme->logo_path->value, $form->edit_theme->logo_path->value);
 
         $this->save_item_state("slideshow_time", $form->edit_theme->slideshow_time->value != 5000, filter_var($form->edit_theme->slideshow_time->value, FILTER_VALIDATE_INT, array('options' => array('default' => 5000, 'min_range' => 1000))));
 
