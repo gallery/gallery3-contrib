020e9b9cf0ad847890c0322d8f15c791ce5e870d
shadlaws <shad@shadlaws.com>
2012-12-12 22:59:46 +0100

    forgot to add these files to the previous commit... oops.

---
+++ 3.0/modules/image_optimizer/helpers/MY_gallery_graphics.php
@@ -0,0 +1,72 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2012 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+class gallery_graphics extends gallery_graphics_Core {
+  
+  // this is copied from modules/gallery/helpers/gallery_graphics.php
+  
+  /**
+   * Rotate an image.  Valid options are degrees
+   *
+   * @param string     $input_file
+   * @param string     $output_file
+   * @param array      $options
+   */
+  static function rotate($input_file, $output_file, $options) {
+    graphics::init_toolkit();
+
+    module::event("graphics_rotate", $input_file, $output_file, $options);
+
+    // BEGIN mod to original function
+    $image_info = getimagesize($input_file); // [0]=w, [1]=h, [2]=type (1=GIF, 2=JPG, 3=PNG)
+    if (module::get_var("image_optimizer","rotate_jpg") || $image_info[2] == 2) {
+      // rotate_jpg enabled, the file is a jpg.  get args
+      $path = module::get_var("image_optimizer", "path_jpg");
+      $exec_args  = " -rotate ";
+      $exec_args .= $options["degrees"] > 0 ? $options["degrees"] : $options["degrees"]+360;
+      $exec_args .= " -copy all -optimize -outfile ";
+      // run it - from input_file to tmp_file
+      $tmp_file = image_optimizer::make_temp_name($output_file);
+      exec(escapeshellcmd($path) . $exec_args . escapeshellarg($tmp_file) . " " . escapeshellarg($input_file), $exec_output, $exec_status);
+      if ($exec_status || !filesize($tmp_file)) {
+        // either a blank/nonexistant file or an error - log an error and pass to normal function
+        Kohana_Log::add("error", "image_optimizer rotation failed on ".$output_file);
+        unlink($tmp_file);
+      } else {
+        // worked - move temp to output
+        rename($tmp_file, $output_file);
+        $status = true;
+      }
+    }
+    if (!$status) {
+      // we got here if we weren't supposed to use jpegtran or if jpegtran failed
+    // END mod to original function
+    
+      Image::factory($input_file)
+        ->quality(module::get_var("gallery", "image_quality"))
+        ->rotate($options["degrees"])
+        ->save($output_file);
+
+    // BEGIN mod to original function
+    }
+    // END mod to original function
+
+    module::event("graphics_rotate_completed", $input_file, $output_file, $options);
+  }
+}
---
+++ 3.0/modules/image_optimizer/helpers/image_optimizer_installer.php
@@ -0,0 +1,75 @@
+<?php defined("SYSPATH") or die("No direct script access.");
+/**
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2012 Bharat Mediratta
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+class image_optimizer_installer {
+  static function install() {
+    $defaults = array('jpg' => '1', 'png' => '2', 'gif' => '1');
+    foreach ($defaults as $type => $optlevel) {
+      // set default path as the pre-compiled versions in the lib
+      module::set_var("image_optimizer", "path_".$type, MODPATH."image_optimizer/lib/".image_optimizer::tool_name($type));
+      // check config status (also sets configstatus_ variables and ensures that the permissions are set correctly)
+      image_optimizer::tool_status($type);
+      // set default optimization levels
+      module::set_var("image_optimizer", "optlevel_thumb_".$type, $optlevel);
+      module::set_var("image_optimizer", "optlevel_resize_".$type, $optlevel);
+    }
+    module::set_var("image_optimizer", "rotate_jpg", true);
+    module::set_var("image_optimizer", "enable_thumb", true);
+    module::set_var("image_optimizer", "enable_resize", true);
+    module::set_var("image_optimizer", "update_mode_thumb", false);
+    module::set_var("image_optimizer", "update_mode_resize", false);
+    module::set_var("image_optimizer", "metastrip_thumb", true);
+    module::set_var("image_optimizer", "convert_thumb_png", "jpg");
+    module::set_var("image_optimizer", "convert_resize_png", false);
+    module::set_var("image_optimizer", "convert_thumb_gif", "jpg");
+    module::set_var("image_optimizer", "convert_resize_gif", false);
+    module::set_var("image_optimizer", "metastrip_resize", false);
+    module::set_var("image_optimizer", "progressive_thumb", false);
+    module::set_var("image_optimizer", "progressive_resize", true);
+    module::set_version("image_optimizer", 1);
+    image_optimizer::add_image_optimizer_rule("thumb");
+    image_optimizer::add_image_optimizer_rule("resize");
+  }
+  
+  static function activate() {
+    // add graphics rules if enabled
+    if (module::get_var("image_optimizer", "enable_thumb")) {
+      image_optimizer::add_image_optimizer_rule("thumb");
+    }
+    if (module::get_var("image_optimizer", "enable_resize")) {
+      image_optimizer::add_image_optimizer_rule("resize");
+    }
+  }
+
+  static function deactivate() {
+    // ensure that update modes are disabled
+    image_optimizer::deactivate_update_mode("thumb");
+    image_optimizer::deactivate_update_mode("resize");
+    // remove graphics rules
+    image_optimizer::remove_image_optimizer_rule("thumb");
+    image_optimizer::remove_image_optimizer_rule("resize");
+  }
+  
+  static function uninstall() {
+    // deactivate
+    module::deactivate("image_optimizer");
+    // delete vars from database
+    module::clear_all_vars("image_optimizer");
+  }
+}
