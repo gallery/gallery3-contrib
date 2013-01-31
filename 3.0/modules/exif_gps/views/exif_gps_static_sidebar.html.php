3898c85995b5a72b30ec8db0223c18ff0b04b1af
Mike Miller <github@mikeage.net>
2013-01-31 12:46:08 +0200

    Replace http:// links with protocol relative links.
    Without this, if the parent page is accessed via https, many browsers
    will refuse to run the insecure JavaScript

--- 3.0/modules/exif_gps/views/exif_gps_static_sidebar.html.php
+++ 3.0/modules/exif_gps/views/exif_gps_static_sidebar.html.php
@@ -5,4 +5,4 @@
     $map_api_key = "&key=" . module::get_var("exif_gps", "googlemap_api_key");
   }
 ?>
-<img src="http://maps.google.com/maps/api/staticmap?center=<?=$latitude; ?>,<?=$longitude; ?>&zoom=<?= module::get_var("exif_gps", "sidebar_zoom"); ?>&size=205x214&maptype=<?=$sidebar_map_type ?>&markers=color:red|color:red|<?=$latitude; ?>,<?=$longitude; ?><?= $map_api_key; ?>&sensor=false">
+<img src="//maps.google.com/maps/api/staticmap?center=<?=$latitude; ?>,<?=$longitude; ?>&zoom=<?= module::get_var("exif_gps", "sidebar_zoom"); ?>&size=205x214&maptype=<?=$sidebar_map_type ?>&markers=color:red|color:red|<?=$latitude; ?>,<?=$longitude; ?><?= $map_api_key; ?>&sensor=false">
