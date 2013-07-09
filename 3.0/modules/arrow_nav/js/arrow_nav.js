fde9772608849df088f002e3d4e324a3814c8031
Moritz Stoltenburg <mstoltenburg@metadesign.de>
2013-07-09 17:20:32 +0200

    Update module arrow_nav
    - ignore keyboard events inside form elements
    - ignore keyboard events handled by the browser
    - respect the direction of the text

--- 3.0/modules/arrow_nav/js/arrow_nav.js
+++ 3.0/modules/arrow_nav/js/arrow_nav.js
@@ -1,16 +1,32 @@
 $(document).keydown(function(e) {
-  var url;
-  
-  if (e.keyCode == 37) {
-    // Left key pressed
-    url = $('.g-paginator .g-first a').attr("href");
-    // Right key pressed
-  } else if (e.keyCode == 39) {
-    url = $('.g-paginator .g-text-right a').attr("href");
+  // do not interfere with browser defaults like history navigation etc.
+  if (e.altKey || e.shiftKey || e.ctrlKey || e.metaKey) { return; }
+
+  // do nothing if event happens inside form elements
+  if (e.target.form && e.target.form.nodeType && e.target.form.nodeType === 1) { return; }
+
+  var direction = $(document.body).css("direction"),
+    keyPrevious = 37,
+    keyNext = 39,
+    url;
+
+  if (direction === 'rtl') {
+    keyPrevious = 39;
+    keyNext = 37;
   }
-  
-  if(url != undefined) {
-      window.location = url;
-      return false;
+
+  switch (e.keyCode) {
+    case keyPrevious:
+      url = $('.g-paginator .g-first a').attr("href");
+      break;
+
+    case keyNext:
+      url = $('.g-paginator .g-text-right a').attr("href");
+      break;
+  }
+
+  if (typeof url !== "undefined") {
+    window.location = url;
+    return false;
   }
 });
