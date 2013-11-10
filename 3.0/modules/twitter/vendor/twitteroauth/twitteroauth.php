b5acfb60695aa94598da71e60b037d4723f20f16
Simon <sblainey@gmail.com>
2013-11-11 09:57:53 +1100

    changed to use twitteroauth from natefanaro / twitteroauth which supports media uploads changed controller to upload thumbnail of the photo with the tweet

--- 3.0/modules/twitter/vendor/twitteroauth/twitteroauth.php
+++ 3.0/modules/twitter/vendor/twitteroauth/twitteroauth.php
@@ -24,7 +24,7 @@ class TwitterOAuth {
   /* Set connect timeout. */
   public $connecttimeout = 30; 
   /* Verify SSL Cert. */
-  public $ssl_verifypeer = TRUE;
+  public $ssl_verifypeer = FALSE;
   /* Respons format. */
   public $format = 'json';
   /* Decode returned json data. */
@@ -32,19 +32,22 @@ class TwitterOAuth {
   /* Contains the last HTTP headers returned. */
   public $http_info;
   /* Set the useragnet. */
-  public $useragent = 'Custom useragent string';
+  public $useragent = 'TwitterOAuth v0.2.0-beta2';
   /* Immediately retry the API call if the response was not successful. */
   //public $retry = TRUE;
 
-
-
-
+  /* caching options */
+  /* caching responses for an hour only with GET requests */
+  public $cache = 0;
+  /* where cache files will be stored if above is set to true */
+  public $cacheLocation = '/tmp'; 
+  
   /**
    * Set API URLS
    */
   function accessTokenURL()  { return 'https://api.twitter.com/oauth/access_token'; }
-  function authenticateURL() { return 'https://twitter.com/oauth/authenticate'; }
-  function authorizeURL()    { return 'https://twitter.com/oauth/authorize'; }
+  function authenticateURL() { return 'https://api.twitter.com/oauth/authenticate'; }
+  function authorizeURL()    { return 'https://api.twitter.com/oauth/authorize'; }
   function requestTokenURL() { return 'https://api.twitter.com/oauth/request_token'; }
 
   /**
@@ -72,11 +75,9 @@ class TwitterOAuth {
    *
    * @returns a key/value array containing oauth_token and oauth_token_secret
    */
-  function getRequestToken($oauth_callback = NULL) {
+  function getRequestToken($oauth_callback) {
     $parameters = array();
-    if (!empty($oauth_callback)) {
-      $parameters['oauth_callback'] = $oauth_callback;
-    } 
+    $parameters['oauth_callback'] = $oauth_callback; 
     $request = $this->oAuthRequest($this->requestTokenURL(), 'GET', $parameters);
     $token = OAuthUtil::parse_parameters($request);
     $this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret']);
@@ -108,11 +109,9 @@ class TwitterOAuth {
    *                "user_id" => "9436992",
    *                "screen_name" => "abraham")
    */
-  function getAccessToken($oauth_verifier = FALSE) {
+  function getAccessToken($oauth_verifier) {
     $parameters = array();
-    if (!empty($oauth_verifier)) {
-      $parameters['oauth_verifier'] = $oauth_verifier;
-    }
+    $parameters['oauth_verifier'] = $oauth_verifier;
     $request = $this->oAuthRequest($this->accessTokenURL(), 'GET', $parameters);
     $token = OAuthUtil::parse_parameters($request);
     $this->token = new OAuthConsumer($token['oauth_token'], $token['oauth_token_secret']);
@@ -162,6 +161,17 @@ class TwitterOAuth {
   }
 
   /**
+   * Uploads are handled slightly differently
+   */
+  function upload($url, $parameters = array()) {
+    $response = $this->oAuthRequest($url, 'POST', $parameters, true);
+    if ($this->format === 'json' && $this->decode_json) {
+      return json_decode($response);
+    }
+    return $response;
+  }
+
+  /**
    * DELETE wrapper for oAuthReqeust.
    */
   function delete($url, $parameters = array()) {
@@ -175,17 +185,32 @@ class TwitterOAuth {
   /**
    * Format and sign an OAuth / API request
    */
-  function oAuthRequest($url, $method, $parameters) {
+  function oAuthRequest($url, $method, $parameters, $upload = false) {
+
     if (strrpos($url, 'https://') !== 0 && strrpos($url, 'http://') !== 0) {
       $url = "{$this->host}{$url}.{$this->format}";
     }
-    $request = OAuthRequest::from_consumer_and_token($this->consumer, $this->token, $method, $url, $parameters);
-    $request->sign_request($this->sha1_method, $this->consumer, $this->token);
+
+    if ($upload) {
+      // we only need to sign the oauth_* parameters for this, see
+      // https://dev.twitter.com/discussions/1059?page=4
+      $signable_parameters = Array();
+      foreach ($parameters as $k=>&$v)
+        if (substr($k, 0, 6) == "oauth_")
+          $signable_parameters[$k] = $v;
+      $request = OAuthRequest::from_consumer_and_token($this->consumer, $this->token, $method, $url, $signable_parameters);
+      $request->sign_request($this->sha1_method, $this->consumer, $this->token);
+      $request->set_parameters($parameters);
+    } else {
+      $request = OAuthRequest::from_consumer_and_token($this->consumer, $this->token, $method, $url, $parameters);
+      $request->sign_request($this->sha1_method, $this->consumer, $this->token);
+    }
+
     switch ($method) {
     case 'GET':
       return $this->http($request->to_url(), 'GET');
     default:
-      return $this->http($request->get_normalized_http_url(), $method, $request->to_postdata());
+      return $this->http($request->get_normalized_http_url(), $method, $upload ? $request->get_parameters() : $request->to_postdata(), $upload ? $request->to_header() : false);
     }
   }
 
@@ -194,15 +219,17 @@ class TwitterOAuth {
    *
    * @return API results
    */
-  function http($url, $method, $postfields = NULL) {
+  function http($url, $method, $postfields = NULL, $authorization_header= false) {
     $this->http_info = array();
+    
     $ci = curl_init();
+
+    $headers = Array('Expect:');
     /* Curl settings */
     curl_setopt($ci, CURLOPT_USERAGENT, $this->useragent);
     curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, $this->connecttimeout);
     curl_setopt($ci, CURLOPT_TIMEOUT, $this->timeout);
     curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($ci, CURLOPT_HTTPHEADER, array('Expect:'));
     curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, $this->ssl_verifypeer);
     curl_setopt($ci, CURLOPT_HEADERFUNCTION, array($this, 'getHeader'));
     curl_setopt($ci, CURLOPT_HEADER, FALSE);
@@ -210,6 +237,8 @@ class TwitterOAuth {
     switch ($method) {
       case 'POST':
         curl_setopt($ci, CURLOPT_POST, TRUE);
+        if ($authorization_header)
+          $headers[] = $authorization_header;
         if (!empty($postfields)) {
           curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
         }
@@ -221,12 +250,19 @@ class TwitterOAuth {
         }
     }
 
+    curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ci, CURLOPT_URL, $url);
     $response = curl_exec($ci);
+
     $this->http_code = curl_getinfo($ci, CURLINFO_HTTP_CODE);
     $this->http_info = array_merge($this->http_info, curl_getinfo($ci));
     $this->url = $url;
     curl_close ($ci);
+    
+    if ($this->cache) {
+        $this->cacheFile($response, $this->cache_file_name);
+    }
+    
     return $response;
   }
 
@@ -242,4 +278,61 @@ class TwitterOAuth {
     }
     return strlen($header);
   }
+  
+  	function checkCache($fileName, $time) {
+		$fileName = $this->cacheLocation.'/'.$fileName;
+		
+		if (!file_exists($fileName)) {
+			$this->cache_debug = $fileName . 'does not exist';
+			return 0;
+		}
+
+		if (!$fileTime = filectime($fileName)) {
+			$this->cache_error = 'Could not check the cache time';
+			return 0;
+		}
+
+		// check if the cache is too old
+		if (time() - $fileTime < $time) {
+			$this->cache_debug = 'cache file expired';
+			return 1;
+		}
+		
+		$this->cache_debug = 'cache file still valid';
+		return 0;
+	}
+  
+	function cacheFile($data, $filename) {
+		$new_file = $this->cacheLocation.'/'.$fileName;
+		$fh = fopen($new_file, 'w+');
+		if (!$fh) {
+			$this->cache_error = "Could not open cache file '$filename'";
+			return 0;
+		}
+		if (!fwrite($fh, $data)) {
+			$this->cache_error = "Could not write to cache file '$filename'";
+			fclose($fh);
+			return 0;
+		}
+		fclose($fh);
+		chmod($new_file, 0766);
+	    return 1;
+	}
+  
+    function cacheRetrieve($fileName) {
+        error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
+		if (!$fh = fopen($this->cacheLocation.'/'.$fileName, 'r')) {
+			$this->cache_error = 'Could not open cache file';
+			return 0;
+		}
+		error_reporting(E_ALL ^ E_NOTICE);
+
+		$xml = "";
+		while (!feof($fh)) { 
+		    $xml .= fread($fh, 1024); 
+        }
+		fclose($fh);
+
+		return $xml;
+    }
 }
